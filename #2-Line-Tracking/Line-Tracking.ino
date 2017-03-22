7d694b888edca90d9c7df0e6c622a611ff5c22e1
Li <lxyangus91133@gmail.com>
2017-03-21 23:42:41 -0400

    add comment

--- #2-Line-Tracking/Line-Tracking.ino
+++ #2-Line-Tracking/Line-Tracking.ino
@@ -1,3 +1,4 @@
+//Line Tracking v1.0
 //pins of right motors.
 int RT1 = 6;
 int RT2 = 7;
@@ -29,44 +30,24 @@ void moveBackward() {
         digitalWrite(LT1, HIGH);
         digitalWrite(LT2, LOW);
 }
-///*Breaking one side for turning - Round turn*/
-////function to turn right.
-//void turnRight() {
-//        analogWrite(ENR, SPD);
-//        analogWrite(ENL, SPD);
-//        digitalWrite(RT1, LOW);
-//        digitalWrite(RT2, LOW);
-//        digitalWrite(LT1, LOW);
-//        digitalWrite(LT2, HIGH);
-//}
-////function to turn left.
-//void turnLeft() {
-//        analogWrite(ENR, SPD);
-//        analogWrite(ENL, SPD);
-//        digitalWrite(RT1, HIGH);
-//        digitalWrite(RT2, LOW);
-//        digitalWrite(LT1, LOW);
-//        digitalWrite(LT2, LOW);
-//}
-/*Reversing one side for turing - Sharpe turn*/
 // //function to turn right.
- void turnRight() {
-         analogWrite(ENR, SPD);
-         analogWrite(ENL, SPD);
-         digitalWrite(RT1, LOW);
-         digitalWrite(RT2, HIGH);
-         digitalWrite(LT1, LOW);
-         digitalWrite(LT2, HIGH);
- }
- //function to turn left.
- void turnLeft() {
-         analogWrite(ENR, SPD);
-         analogWrite(ENL, SPD);
-         digitalWrite(RT1, HIGH);
-         digitalWrite(RT2, LOW);
-         digitalWrite(LT1, HIGH);
-         digitalWrite(LT2, LOW);
- }
+void turnRight() {
+        analogWrite(ENR, SPD);
+        analogWrite(ENL, SPD);
+        digitalWrite(RT1, LOW);
+        digitalWrite(RT2, HIGH);
+        digitalWrite(LT1, LOW);
+        digitalWrite(LT2, HIGH);
+}
+//function to turn left.
+void turnLeft() {
+        analogWrite(ENR, SPD);
+        analogWrite(ENL, SPD);
+        digitalWrite(RT1, HIGH);
+        digitalWrite(RT2, LOW);
+        digitalWrite(LT1, HIGH);
+        digitalWrite(LT2, LOW);
+}
 //function to stop.
 void stop() {
         digitalWrite(ENR, LOW);
@@ -83,40 +64,7 @@ void setup() {
         pinMode(ENL, OUTPUT);
         stop();
 }
-//void loop() {
-////lable hell.
-//hell:
-//        //'1' means detection is true.
-//        //get detection.
-//        right = digitalRead(10);
-//        mid = digitalRead(4);
-//        left = digitalRead(2);
-//        //go forward if only mid has detection.
-//        if(mid == 1 && left == 0 && right == 0) {
-//                moveForward();
-//                goto hell;
-//        }
-//        //discard mid, turn left if left has detection.
-//        if(left == 1 && right == 0) {
-//                turnLeft();
-//                goto hell;
-//        }
-//        //dicard mid, turn right if right has detection.
-//        if(left ==0 && right == 1) {
-//                turnRight();
-//                goto hell;
-//        }
-//        //deal with intersection, go forward if all the sensor has detection.
-//        if(mid == 1 && left == 1 && right == 1) {
-//                moveForward();
-//                goto hell;
-//        }
-//        if(mid == 0 && left == 0 && right == 0) {
-//                moveBackward();
-//                goto hell;
-//        }
-//}
-//get rid of 'goto'
+//main loop
 void loop() {
         //'1' means detection is true.
         //get detection.
@@ -130,60 +78,63 @@ void loop() {
         //discard mid, turn left if left has detection.
         if (left == 1 && right == 0) {
                 turnLeft();
-                // delay(20);
-                // stop();
-                // delay(5);
+                //keep turn left unless left has no detection.
                 while (1) {
+                        //update detection.
                         right = digitalRead(10);
                         mid = digitalRead(4);
                         left = digitalRead(2);
+                        //break the while loop if left has no detecton and go farward a bit.
                         if (left == 0) {
-                          moveForward();
-                          delay(20);
-                          break;
+                                moveForward();
+                                delay(20);
+                                break;
                         }
+                        //in case car is confuing when encounter intersection.
                         if(left == 1 && mid == 1 && right == 1) {
                                 moveForward();
                                 break;
                         }
+                        //else keep turn left.
                         else {
                                 turnLeft();
-                                // delay(100);
                         }
                 }
         }
         //dicard mid, turn right if right has detection.
         if (left == 0 && right == 1) {
                 turnRight();
-                // delay(20);
-                // stop();
-                // delay(5);
+                //keep turn right unless left has no detection.
                 while (1) {
+                        //update detection.
                         right = digitalRead(10);
                         mid = digitalRead(4);
                         left = digitalRead(2);
+                        //break the while loop if right has no detecton and go farward a bit.
                         if (right == 0) {
                                 moveForward();
                                 delay(20);
                                 break;
                         }
+                        //in case car is confuing when encounter intersection.
                         if(left == 1 && mid == 1 && right == 1) {
                                 moveForward();
                                 break;
                         }
+                        //else keep turn right.
                         else {
                                 turnRight();
-                                // delay(100);
                         }
                 }
         }
+        //maybe unnecessary
         //deal with intersection, go forward if all the sensor has detection.
         if(mid == 1 && left == 1 && right == 1) {
                 moveForward();
         }
+        //maybe unnecessary.
         if(mid == 0 && left == 0 && right == 0) {
                 moveForward();
                 delay(10);
-
         }
 }
